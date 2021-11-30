3d1246845c3d574c71cd16cd7ba7152228bf9ff2
SterileThunder <93866150+thunderbowel666@users.noreply.github.com>
2021-11-30 22:39:23 +0900

    Add files via upload

---
+++ class Apple:.py
@@ -0,0 +1,10 @@
+class Apple:
+    def __init__(self, c, t, s, w):
+        self.color = c
+        self.taste = t
+        self.shape = s
+        self.weight = w
+        print("Created!")
+
+apl = Apple("red", "sweet", "ball", "heavy")
+print(apl)
